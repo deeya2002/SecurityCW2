83dd2da76a6e21b83735dbceaa5186e7fc8344d8
deeya2002 <diyadangol2002@gmail.com>
2024-08-17 11:35:38 +0545

    made changes

--- BackEnd/utils/logActivity.js
+++ BackEnd/utils/logActivity.js
@@ -1,22 +1,3 @@
-// const AuditLog = require("../model/auditlogModel");
-
-// async function logActivity(userId, action, details) {
-//     try {
-//         const auditLog = new AuditLog({
-//             userId: userId,
-//             action: action,
-//             details: details,
-//         });
-//         await auditLog.save();
-//         console.log('Activity logged:', action);
-//     } catch (err) {
-//         console.error('Error logging activity:', err);
-//     }
-// }
-
-// module.exports = logActivity;
-
-
 const AuditLog = require("../model/auditlogModel");
 const User = require("../model/usermodel");  // Import the User model
 
